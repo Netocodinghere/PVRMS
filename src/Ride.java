d81fa30da48f48ceeb3e1153894a5be70e7a9952
Netocodinghere <nanierobi@gmail.com>
2025-07-30 23:29:33 +0100

    Creation of Ride class with attributes and methods for type, capacity, operator, and riders

---
+++ src/Ride.java
@@ -0,0 +1,75 @@
+import java.util.ArrayList;
+
+public class Ride{
+    public String type;
+    public int capacity;
+    public boolean isOpened;
+    private Employee operator;
+    private ArrayList<Visitor> riders;
+    
+    Ride(){
+        this.type = "";
+        this.capacity = 0;
+        this.isOpened = false;
+        this.operator = null;
+        this.riders = null;
+    
+
+    }
+
+    Ride(String type, int capacity, Employee operator, ArrayList<Visitor> riders ){
+        this.type = type;
+        this.capacity = capacity;
+        this.isOpened = true;
+        this.operator = operator;
+        this.riders = riders;
+    }
+
+    public void setType(String type){
+        this.type = type;
+    }
+
+    public String getType(){
+        return this.type;
+    }
+
+    public void setCapacity(int capacity){
+        this.capacity = capacity;
+    }
+
+    public int getCapacity(){
+        return this.capacity;
+    }
+
+        public void setOperator(Employee operator){
+        this.operator = operator;
+    }
+
+    public Employee getOperator(){
+        return this.operator;
+    }
+
+    public void setRiders(ArrayList<Visitor> riders){
+        this.riders = riders;
+    }
+
+    public ArrayList<Visitor> getRiders(){
+        return this.riders;
+    }
+
+    public void setOpened(boolean isOpened){
+        this.isOpened = isOpened;
+    }
+
+    public boolean getOpened(){
+        return this.isOpened;
+    }
+
+    public void addNewRider(Visitor rider){
+          this.riders.add(rider);
+    }
+
+    
+
+
+}
