22c07e8538d458a927f46d01059dd01fc411f60a
Netocodinghere <nanierobi@gmail.com>
2025-08-02 16:54:17 +0100

    add(Ride):addition of VisitorComparator for visitor sorting

--- src/VisitorComparator.java
+++ src/VisitorComparator.java
@@ -1,5 +1,22 @@
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Comparator;
+import java.lang.Integer;
+
+public class VisitorComparator implements  Comparator<Visitor>{
+
+    public int compare(Visitor v1, Visitor v2) {
+        int ageCompare=Integer.compare(v1.getAge(), v2.getAge());
+        if(ageCompare!=0){
+            return ageCompare;
+        }
+        return v1.getName().compareTo(v2.getName());
+
+    }
+
+
+
+}
 
 interface RideInterface{
     void addVisitorToQueue(Visitor visitor);
